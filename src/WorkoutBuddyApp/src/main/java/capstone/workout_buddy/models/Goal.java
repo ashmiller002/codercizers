1fe9b7591d65508105417bf168e1921c6d8bf6da
Ryan Thompson <rthomp2113@gmail.com>
2021-08-25 13:22:41 -0400

    ActivityLevel, Goal, Program models and mappers added

---
+++ src/WorkoutBuddyApp/src/main/java/capstone/workout_buddy/models/Goal.java
@@ -0,0 +1,23 @@
+package capstone.workout_buddy.models;
+
+public class Goal {
+
+    private int goalId;
+    private String goalName;
+
+    public int getGoalId() {
+        return goalId;
+    }
+
+    public void setGoalId(int goalId) {
+        this.goalId = goalId;
+    }
+
+    public String getGoalName() {
+        return goalName;
+    }
+
+    public void setGoalName(String goalName) {
+        this.goalName = goalName;
+    }
+}
