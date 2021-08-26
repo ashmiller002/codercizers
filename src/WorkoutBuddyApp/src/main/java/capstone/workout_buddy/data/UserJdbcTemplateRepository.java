41575294178a9fd6594b79eec256419037e40e7d
rthomp2113 <78567677+rthomp2113@users.noreply.github.com>
2021-08-26 10:50:25 -0400

    Update UserJdbcTemplateRepository.java

--- src/WorkoutBuddyApp/src/main/java/capstone/workout_buddy/data/UserJdbcTemplateRepository.java
+++ src/WorkoutBuddyApp/src/main/java/capstone/workout_buddy/data/UserJdbcTemplateRepository.java
@@ -4,5 +4,40 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 public class UserJdbcTemplateRepository implements UserRepository{
-//    private final JdbcTemplate jdbcTemplate;
-}
+  
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserJdbcTemplateRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+    @Override
+    public User findByUserId(int userId) {
+        final String sql = "select user_id, first_name, last_name, date_birth, email, program_id, " +
+                "login_id from `user` where user_id = ?;";
+
+        User user = jdbcTemplate.query(sql, new UserMapper(), userId).stream()
+                .findFirst().orElse(null);
+
+        return user;
+    }
+
+    
+
+    @Override
+    public User findByLoginId() {
+        return null;
+    }
+
+    @Override
+    public User add(User user) {
+        return null;
+    }
+
+    @Override
+    public boolean update(User user) {
+        return false;
+    }
+    //private final JdbcTemplate jdbcTemplate;
+
