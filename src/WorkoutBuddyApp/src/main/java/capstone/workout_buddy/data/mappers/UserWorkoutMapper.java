1da94831efdefe9b9d5b1882d5606fe14332437e
Ryan Thompson <rthomp2113@gmail.com>
2021-08-30 12:13:25 -0400

    fixed userId error

--- src/WorkoutBuddyApp/src/main/java/capstone/workout_buddy/data/mappers/UserWorkoutMapper.java
+++ src/WorkoutBuddyApp/src/main/java/capstone/workout_buddy/data/mappers/UserWorkoutMapper.java
@@ -12,13 +12,21 @@ public class UserWorkoutMapper implements RowMapper<UserWorkout> {
     public UserWorkout mapRow(ResultSet resultSet, int i) throws SQLException{
         UserWorkout userWorkout = new UserWorkout();
 
-        userWorkout.getWorkout().setCategoryId(resultSet.getInt("category_id"));
-        userWorkout.getWorkout().setWorkoutName(resultSet.getString("workout_name"));
-        userWorkout.getWorkout().setWorkoutId(resultSet.getInt("workout_id"));
-        userWorkout.getWorkout().setImageUrl(resultSet.getString("image_url"));
-        userWorkout.getWorkout().setWorkoutStatus(resultSet.getString("workout_status"));
+
+//        userWorkout.getWorkout().setCategoryId(resultSet.getInt("category_id"));
+//        userWorkout.getWorkout().setWorkoutName(resultSet.getString("workout_name"));
+//        userWorkout.getWorkout().setWorkoutId(resultSet.getInt("workout_id"));
+//        userWorkout.getWorkout().setImageUrl(resultSet.getString("image_url"));
+//        userWorkout.getWorkout().setWorkoutStatus(resultSet.getString("workout_status"));
         userWorkout.setWorkoutDate(resultSet.getDate("workout_date").toLocalDate());
         userWorkout.setUserId(resultSet.getInt("user_id"));
+        userWorkout.setUserWorkoutId(resultSet.getInt("user_workout_id"));
+
+
+        WorkoutMapper workoutMapper = new WorkoutMapper();
+        userWorkout.setWorkout(workoutMapper.mapRow(resultSet, i));
+
+
 
         return userWorkout;
 
